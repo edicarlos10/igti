cddad8297308bcf13780043d06d430e23ef532bb
Edi <edi.carlos.do.ec@gmail.com>
2022-05-03 20:24:33 -0300

    added actions to main activity and life cicle with logs

--- app/src/main/java/com/cide/igti/MainActivity.kt
+++ app/src/main/java/com/cide/igti/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.cide.igti
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.cide.igti.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val tag: String = this.javaClass.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        Log.i(tag, "This is onCreate")
+
+        binding.let {
+            it.buttonClickHere.setOnClickListener {
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag, "This is onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag, "This is onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(tag, "This is onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag, "This is onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag, "This is onDestroy")
     }
 }
